5e69a727e95f65c7dd54d6d164a13f56d5e084a2
stefnotch <brandmairstefan@gmail.com>
2022-02-26 14:37:15 +0100

    Generate .d.ts files

---
+++ build/index.d.ts
@@ -0,0 +1,44 @@
+import { Vector2 } from 'three';
+export declare class InteractiveObject {
+    target: THREE.Object3D;
+    name: string;
+    intersected: boolean;
+    wasIntersected: boolean;
+    distance: number;
+    constructor(target: THREE.Object3D, name: string);
+}
+export declare class InteractiveEvent {
+    type: string;
+    cancelBubble: boolean;
+    originalEvent: Event | null;
+    coords: Vector2;
+    distance: number;
+    intersected: boolean;
+    constructor(type: string, originalEvent?: Event | null);
+    stopPropagation(): void;
+}
+export declare class InteractionManager {
+    renderer: THREE.Renderer;
+    camera: THREE.Camera;
+    domElement: HTMLElement;
+    mouse: Vector2;
+    supportsPointerEvents: boolean;
+    interactiveObjects: InteractiveObject[];
+    raycaster: THREE.Raycaster;
+    treatTouchEventsAsMouseEvents: boolean;
+    constructor(renderer: THREE.Renderer, camera: THREE.Camera, domElement: HTMLElement);
+    dispose: () => void;
+    add: (object: THREE.Object3D, childNames?: string[]) => void;
+    remove: (object: THREE.Object3D, childNames?: string[]) => void;
+    update: () => void;
+    checkIntersection: (object: InteractiveObject) => void;
+    onDocumentMouseMove: (mouseEvent: MouseEvent | PointerEvent) => void;
+    onTouchMove: (touchEvent: TouchEvent) => void;
+    onMouseClick: (mouseEvent: MouseEvent) => void;
+    onMouseDown: (mouseEvent: MouseEvent | PointerEvent) => void;
+    onTouchStart: (touchEvent: TouchEvent) => void;
+    onMouseUp: (mouseEvent: MouseEvent | PointerEvent) => void;
+    onTouchEnd: (touchEvent: TouchEvent) => void;
+    dispatch: (object: InteractiveObject, event: InteractiveEvent) => void;
+    mapPositionToPoint: (point: Vector2, x: number, y: number) => void;
+}
